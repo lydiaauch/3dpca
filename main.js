8332afe148faaa90728451b81fe19ae8073e7e75
Lydia Auch <lydiaauch@Lydias-Mac-mini.local>
2016-09-09 10:07:15 -0400

    wrap svg and canvas in divs

---
+++ main.js
@@ -0,0 +1,224 @@
+var scene, camera, renderer, plotContainer;
+var uploader = document.getElementById("uploader");
+var reader = new FileReader();
+var data;
+
+/*   GET DATA POINTS    */
+function getPts(x) {
+    var unfiltered = [];
+    x.forEach(function (d, i) {
+        unfiltered[i] = {
+            e1: +d[1],
+            e2: +d[2],
+            e3: +d[3],
+            e4: +d[4], 
+            e5: +d[5], 
+            e6: +d[6],
+            r: d[d.length - 1],
+            i: i
+        };
+    });
+    return unfiltered;
+}
+
+reader.onload = function(e) {
+    var contents = e.target.result;
+    var rawData = contents.split(/\n/);
+    var tempData = [];
+    for (i = 0; i < rawData.length; i++) {
+        pointData = rawData[i].split(/\s/);
+        setData = pointData.filter(function(e) { return e!==""; });
+        tempData.push(setData);
+    }
+
+    tempData.shift();
+    data = getPts(tempData);
+    scatter(data);
+};
+
+uploader.addEventListener("change", handleFiles, false);
+
+function handleFiles() {
+    var file = this.files[0];
+    reader.readAsText(file);
+
+    if ( scene.getObjectByName('points') ) {
+        scene.remove(scene.getObjectByName('points'));
+        d3.select(".lcontainer").remove();
+    }
+}
+
+init();
+animate();
+
+/*      CREATE SCENE        */
+function init() {
+    scene = new THREE.Scene();
+
+    plotContainer = document.getElementById('plot');
+    document.body.appendChild(plotContainer);
+
+    var PWIDTH = plotContainer.offsetWidth,
+        HEIGHT = plotContainer.offsetHeight;
+
+    renderer = new THREE.WebGLRenderer({antialias:true});
+    renderer.setSize(PWIDTH, HEIGHT);
+    plotContainer.appendChild(renderer.domElement);
+
+    camera = new THREE.PerspectiveCamera(45, PWIDTH / HEIGHT, .1, 10000);
+    camera.position.set(0,10,250);
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+    scene.add(camera);
+
+    window.addEventListener('resize', function() {
+        var PWIDTH = plotContainer.offsetWidth,
+            HEIGHT = plotContainer.offsetHeight,
+            LWIDTH = document.getElementById('legend').offsetWidth;
+
+        renderer.setSize(PWIDTH, HEIGHT);
+        camera.aspect = PWIDTH / HEIGHT;
+        camera.updateProjectionMatrix();
+
+        d3.select(".lcontainer").attr("height", HEIGHT).attr("width", LWIDTH);
+    });
+
+    renderer.setClearColor(0x333F47, 1);
+
+    var light = new THREE.PointLight(0xffffff);
+    light.position.set(-100, 200, 100);
+    scene.add(light);
+
+    function buildAxis(src, dst, colorHex, dashed) {
+        var geom = new THREE.Geometry(), mat;
+
+        if(dashed) {
+            mat = new THREE.LineDashedMaterial({ linewidth: 3, color: colorHex, dashSize: 3, gapSize: 3 });
+        }
+        else {
+            mat = new THREE.LineBasicMaterial({ linewidth: 3, color: colorHex });
+        }
+        geom.vertices.push( src.clone() );
+        geom.vertices.push( dst.clone() );
+        geom.computeLineDistances();
+
+        axis = new THREE.Line( geom, mat, THREE.LineSegments );
+        return axis;
+    }
+
+    function buildAxes(length) {
+        var axes = new THREE.Object3D();
+
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( length, 0, 0 ), 0xA4A4A4, false ) ); // +X
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( -length, 0, 0 ), 0xA4A4A4, true) ); // -X
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, length, 0 ), 0x858585, false ) ); // +Y
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, -length, 0 ), 0x858585, true ) ); // -Y
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, length ), 0xC8C8C8, false ) ); // +Z
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, -length ), 0xC8C8C8, true ) ); // -Z
+        return axes;
+    }
+
+    axes = buildAxes( 1000 );
+    scene.add(axes);
+}
+
+
+/*      RENDER DATA        */
+function scatter(data) {
+    unfiltered = data;
+
+    var xScale = d3.scale.linear()
+        .domain(d3.extent(unfiltered, function (d) {return d.e1; }))
+        .range([-50,50]);
+    var yScale = d3.scale.linear()
+        .domain(d3.extent(unfiltered, function (d) {return d.e2; }))
+        .range([-50,50]);
+    var zScale = d3.scale.linear()
+        .domain(d3.extent(unfiltered, function (d) {return d.e3; }))
+        .range([-50,50]);
+
+    var pointCount = unfiltered.length;
+
+    var categories = [];
+    for (i=0; i<pointCount; i++) {
+        var group = unfiltered[i].r;
+        if (categories.indexOf(group) == -1) {
+            categories.push(unfiltered[i].r);
+        }
+    }
+
+    var color = d3.scale.ordinal()
+        .domain(categories)
+        .range(color_set);
+
+    var geom = new THREE.SphereGeometry(1, 32, 32);
+    points = new THREE.Object3D();
+    points.name = "points";
+    scene.add(points);
+
+    for (var i = 0; i < pointCount; i ++) {
+        var x = xScale(unfiltered[i].e1),
+            y = yScale(unfiltered[i].e2),
+            z = zScale(unfiltered[i].e3);
+        group = unfiltered[i].r;
+
+        var point = new THREE.Mesh(geom, new THREE.MeshBasicMaterial({
+            side: THREE.DoubleSide,
+            color: color(group)
+        }));
+        point.name = group;
+        point.position.set(x, y, z);
+        points.add(point);
+    }
+
+    /*      CREATE LEGEND        */
+    var LWIDTH = document.getElementById('legend').offsetWidth,
+        HEIGHT = document.getElementById('legend').offsetHeight,
+        COLORBLOCK = 15,
+        SPACING = 4;
+
+    var svg = d3.select("#legend").append("svg")
+        .attr("class", "lContainer")
+        .attr("width", LWIDTH + 100)
+        .attr("height", categories.length * 19 + 5);
+
+    legend_data = unfiltered.slice(0,categories.length -1 );
+
+    var legend = svg.selectAll(".legend")
+        .data(legend_data)
+        .enter()
+        .append("g")
+        .attr("class", "ldata")
+        .attr("transform", function(d, i) {
+            while (i < categories.length) {
+                var height = COLORBLOCK + SPACING;
+                var horz = 5;
+                var vert = i * height + 5;
+                return "translate(" + horz + ", " + vert + ")";
+            }
+        });
+
+    legend.append("rect")
+        .attr("width", COLORBLOCK)
+        .attr("height", COLORBLOCK)
+        .style("fill", function(i) {
+            return color_set[i.i];
+        })
+        .style("stroke", function(i) {
+            return color_set[i];
+        });
+
+    legend.append("text")
+        .attr("x", COLORBLOCK + SPACING)
+        .attr("y", COLORBLOCK )
+        .text(function (d, i) { while (i < categories.length) { return categories[i]; }});
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    renderer.render(scene, camera);
+    controls.update();
+}
+
+var color_set = ["#02011b", "#1eff06", "#2401fe", "#ff1902", "#1ffefe", "#fea0ff", "#898105", "#98043d", "#022e9d", "#fddccc", "#0dacff", "#fcff04", "#fe08fb", "#046b52", "#a2fe94", "#ff9303", "#856985", "#4e2901", "#ff04a1", "#018502", "#fd8771", "#840176", "#feec89", "#9555ff", "#ff0155", "#79b2bc", "#a42203", "#8f835f", "#87d607", "#7f88fe", "#d1c8ff", "#13bf8c", "#02427e", "#0a2002", "#c5fed5", "#f572a2", "#4a0232", "#e5bd07", "#315201", "#a66526", "#04f86e", "#8ab265", "#0102b2", "#214a57", "#d44cd4", "#0f0146", "#a55a56", "#9b64b2", "#ffb46a", "#e6a3ba", "#dffe6a", "#fd6b31", "#ac02ff", "#027da8", "#4a3034", "#7c01a7", "#0cfead", "#0bd2ff", "#7fa91c", "#d6f3ff", "#0263fc", "#680204", "#9c4473", "#7a88c8", "#2dc45e", "#da9d81", "#248542", "#bc1474", "#cec189", "#68a888", "#c28808", "#02b708", "#4a503b", "#695815", "#d5414b", "#57326f", "#9357d1", "#ada0a1", "#fe68c9", "#0267c6", "#3c0103", "#0f42ff", "#ff3a83", "#6dfed2", "#ff0ece", "#0cbebc", "#bea2fa", "#470170", "#ddffaa", "#da0822", "#bbc703", "#05818a", "#bb20dd", "#7fb9ea", "#b2cab4", "#65787b", "#597846", "#bb964b", "#bdbb47", "#c96701", "#79ff4b", "#322f4e", "#73273a", "#da85c0", "#93e9e4", "#fefcca", "#c8fe0a", "#53811d", "#8bde9c", "#7d5f57", "#343401", "#1a0f01", "#b55136", "#5036c7", "#c20498", "#fedbf3", "#5a4ca8", "#acd767", "#b84a9e", "#7ad751", "#014319", "#e4818a", "#febcf8", "#7a3405", "#ffee57", "#495c81", "#b795c3", "#7802ce", "#845633", "#d982ff", "#f9c858", "#012224", "#8391b0", "#21062f", "#fd8e51", "#868840", "#26018b", "#054438", "#022978", "#c04d6b", "#790252", "#663b5b", "#61ad45", "#b47086", "#729efe", "#9a0420", "#325cd4", "#b6c1d9", "#270316", "#ff6e82", "#fe5c4d", "#635b9b", "#b2d39b", "#1e6eb0", "#91dffb", "#016302", "#07a4c3", "#ca044f", "#ffcb97", "#ff57ff", "#753326", "#feb228", "#7cd8b6", "#813a9b", "#560358", "#859885", "#b0fe60", "#4fa703", "#c4d07b", "#ffe304", "#9aff02", "#b2837c", "#0e936d", "#cc64ff", "#58525e", "#b59d03", "#d13501", "#846dff", "#d48157", "#021e51", "#3d2e19", "#6e27fd", "#7d77cf", "#66ff8e", "#f8ffee", "#8b6704", "#90a270", "#07d908", "#9f22a4", "#ffb8b2", "#ad7de5", "#c9b299", "#5ab36c", "#a1373e", "#a14501", "#5f6904", "#0e89fd", "#a5d339", "#ad8053", "#823379", "#88578a", "#622ca3", "#d371d3", "#e5c371", "#50755f", "#f04b9c", "#9ab5fb", "#45dff0", "#64e48f", "#f19a3a", "#d1552a", "#899c37", "#06d3b8", "#854e5c", "#ff7cf4", "#3d5224", "#099b41", "#dbf133", "#07afe9", "#333835", "#fe5203", "#06978a", "#fe7605", "#c9508d", "#e3f482", "#d1615e", "#81fd6e", "#5d4b26", "#6e4501", "#4a93d0", "#f244c6", "#040b67", "#fd97bf", "#d40bc9", "#9841d5", "#d5d4d2", "#236429", "#dfbbc0", "#0f04d7", "#346d81", "#0eff51", "#013e64", "#fb456b", "#20d24e", "#0588e0", "#ffaa7f", "#256361", "#ce8a3d", "#85d476", "#633d30", "#03263b", "#d63973", "#ff472d", "#b2ab57", "#16e7ae", "#ffe6a3", "#b48ea2", "#9e1e5b", "#72c1b6", "#9ea802", "#cdfef1", "#d5023a", "#d30efa", "#59938c", "#5e9962", "#4c1729", "#4a287c", "#902914", "#fd81c8", "#c1fd7e", "#730323", "#4c50f5", "#8a8075", "#bc88cd", "#fc9d8f", "#03a828", "#cc7b6b", "#311301", "#a93afb", "#cb6d32", "#5993ac", "#251f28", "#b1609d", "#64da36", "#d2d94e", "#72254f", "#9076b0", "#d0b6d5", "#710689", "#564672", "#ae50be", "#92fee6", "#cbab43", "#dfd8b5", "#5202aa", "#21499c", "#686836", "#390f4b", "#401d3b", "#a7d4d7", "#7966d4", "#ab9a62", "#a96243", "#d16f96", "#dda66d", "#9e1e75", "#78043c", "#bd1e24", "#698b42", "#a4a8d8", "#4e2bdb", "#d34ebb", "#ddc73f", "#8bffba", "#aedc8b", "#501602", "#fb7651", "#5f4cca", "#927635", "#b16c01", "#ac7cfe", "#3fb695", "#bdfeba", "#e60381", "#508e01", "#81bf89", "#9a455a", "#39a7af", "#a902b6", "#587ca9", "#fff3fa", "#fa6a66", "#01b56b", "#8a818e", "#ab759f", "#193203", "#2e593c", "#e633a4", "#8499a0", "#57e3d9", "#b2d8ff", "#0576fe", "#fa872f", "#d2aded", "#dfe8a5", "#444b03", "#fd0126", "#67f802", "#0cfde6", "#475856", "#a9871f", "#b9ef35", "#fd3647", "#d25404", "#dba926", "#dedcfe", "#6f654b", "#308528", "#59c4db", "#020a30", "#2d361a", "#724a95", "#96725a", "#d89495", "#07e384", "#0a47ce", "#468664", "#f0bfa5", "#6de173", "#d3ecce", "#323270", "#574841", "#bd344f", "#562221", "#1746ad", "#e5a847", "#ffa3e9", "#ffc3e6", "#99f5c4", "#536fbc", "#986e78", "#d393ef", "#5b1e54", "#79759f", "#aab9b9", "#b3ba93", "#61dcab", "#decf15", "#0b598b", "#63738b", "#8bb64f", "#e09fcd", "#71e307", "#370258", "#523c01", "#5a9b49", "#fc5d8c", "#7bba04", "#a8e0c8", "#f40363", "#bb5de0", "#ef6efe", "#56021e", "#566a26", "#d85846", "#a797da", "#a8c13e", "#ab2a96", "#925cc0", "#738461", "#6e8c04", "#029d62", "#b59275", "#fb83e0", "#7fbf41", "#fea2b0", "#a4fa3a", "#3c2654", "#313949", "#e3cc61", "#fffa43", "#661677", "#953f2e", "#7734d7", "#197749", "#945779", "#82693e", "#d93e29", "#d76ebb", "#e29201", "#94bd99", "#abba78", "#2fe446", "#1d2418", "#041eb0", "#df0f03", "#5772dc", "#da4ea4", "#88adc8", "#bcf692", "#8b35b3", "#7d7018", "#034904", "#7d41fd", "#dc50fe", "#fdfea9", "#be6371", "#f604b3", "#9696fa", "#d3a69b", "#c01cb1", "#cdab7e", "#0a5676", "#b70301", "#8bb4a9", "#277c70", "#a9a2ba", "#030139", "#311686", "#4d394e", "#875346", "#b23833", "#a664f7", "#fa21e6", "#ff58e9", "#fff879", "#b20254", "#6a70f1", "#defdc4", "#010d12", "#211630", "#05361c", "#703b18", "#9114c6", "#417a2e", "#a39b84", "#f87f80", "#ecadfc", "#7cffff", "#b80937", "#ffddb5", "#570191", "#bc3fd9", "#e56d87", "#52bb46", "#6bb3f8", "#653340", "#112bfe", "#844143", "#de724e", "#439cc8", "#ec975c", "#6ecffc", "#9af675", "#b1f3fe", "#8b5f25", "#4a349b", "#694a53", "#566643", "#696561", "#9f4f21", "#53669d", "#9c4b9f", "#e2496a", "#9e8e35", "#e3702e", "#5bce8b", "#f4cdfe", "#2e1718", "#8e2741", "#773a6b", "#7b4fe3", "#b34af4", "#aa7b2f", "#6397e4", "#ce85a8", "#56c36b", "#a8b957", "#601217", "#884a02", "#50848d", "#7897c9", "#9e9d23", "#beb407", "#08d38a", "#add204", "#c7d467", "#ffc1cf", "#ffd247", "#042448", "#356503", "#abc7fe", "#281b60", "#153a3a", "#661e02", "#4d438d", "#625774", "#db0ee4", "#b77e66", "#f84d59", "#9a87a7", "#4cc02f"]
